214d6a51a012d29d99e7dfc13658555c7791d153
xi <3066994379@qq.com>
2021-07-12 15:27:57 +0800

    first commit.

---
+++ src/cn/day02/JinZhiDemo.java
@@ -0,0 +1,10 @@
+package cn.day02;
+
+public class JinZhiDemo {
+	public static void main(String[] args) {
+		System.out.println(100); //十进制
+		System.out.println(0b100); //二进制
+		System.out.println(0100); //八进制
+		System.out.println(0x100); //十六进制
+	}
+}
